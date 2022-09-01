50c8d92a510cd725e97017acf30b232441f0848e
akifdlhan <112507694+akifdlhan@users.noreply.github.com>
2022-09-01 13:35:19 +0300

    Add files via upload

---
+++ ekolayzer/ekolayzer/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ekolayzer
+{
+    partial class frm_eko
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel1.Location = new System.Drawing.Point(41, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(18, 150);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Yellow;
+            this.panel2.Location = new System.Drawing.Point(68, 23);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(18, 150);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Cyan;
+            this.panel3.Location = new System.Drawing.Point(95, 23);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(18, 150);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel4.Location = new System.Drawing.Point(122, 23);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(18, 150);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Black;
+            this.panel5.Location = new System.Drawing.Point(149, 23);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(18, 150);
+            this.panel5.TabIndex = 1;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel6.Location = new System.Drawing.Point(176, 23);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(18, 150);
+            this.panel6.TabIndex = 1;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Lime;
+            this.panel7.Location = new System.Drawing.Point(203, 23);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(18, 150);
+            this.panel7.TabIndex = 1;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel8.Location = new System.Drawing.Point(230, 23);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(18, 150);
+            this.panel8.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frm_eko
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 195);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frm_eko";
+            this.Text = "Ekolayzer";
+            this.Load += new System.EventHandler(this.frm_eko_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ekolayzer/ekolayzer/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+namespace ekolayzer
+{
+    
+    public partial class frm_eko : Form
+    {
+        public frm_eko()
+        {
+            InitializeComponent();
+        }
+        SoundPlayer player=new SoundPlayer();
+ Random random = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel1.Height =random.Next(150);
+            panel2.Height = random.Next(150);
+            panel3.Height = random.Next(150);
+            panel4.Height = random.Next(150);
+            panel5.Height = random.Next(150);
+            panel6.Height = random.Next(150);
+            panel7.Height = random.Next(150);
+            panel8.Height = random.Next(150);
+        }
+       
+        private void frm_eko_Load(object sender, EventArgs e)
+        {
+            player.SoundLocation = @"C:\Users\MONSTER\Downloads\Kar-Gördüm-Kaydım-_REMIX_2016-2017-MAHMUT-TUNCER....wav";
+            player.Play();
+            timer1.Start();
+            this.BackColor = Color.Black;
+        }
+    }
+}
